c1a65768bb758d06043915a06a40454f2cc72a9a
futpib <futpib@gmail.com>
2018-05-14 23:45:02 +0300

    Update benchmarks

--- index.benchmark.js
+++ index.benchmark.js
@@ -119,3 +119,49 @@ benchmark('With changing context', b => {
 			withLexical({ series: series_ })(series_.length - 1);
 		});
 });
+
+benchmark('With changing context 2', b => {
+	const dConstA = n => d.const({ series }, () => withD(n));
+	const dConstB = n => d.const({ series: series_ }, () => withD(n));
+
+	const dBindA = d.bind({ series }, withD);
+	const dBindB = d.bind({ series: series_ }, withD);
+
+	const thisCallA = n => withThis.call({ series }, n);
+	const thisCallB = n => withThis.call({ series: series_ }, n);
+
+	const thisBindA = withThis.bind({ series });
+	const thisBindB = withThis.bind({ series: series_ });
+
+	const factoriesA = withFactories({ series });
+	const factoriesB = withFactories({ series: series_ });
+
+	const lexicalA = withLexical({ series });
+	const lexicalB = withLexical({ series: series_ });
+
+	return b
+		.add('d+const', () => {
+			dConstA(series.length - 1);
+			dConstB(series.length - 1);
+		})
+		.add('d+bind', () => {
+			dBindA(series.length - 1);
+			dBindB(series.length - 1);
+		})
+		.add('this+call', () => {
+			thisCallA(series.length - 1);
+			thisCallB(series.length - 1);
+		})
+		.add('this+bind', () => {
+			thisBindA(series.length - 1);
+			thisBindB(series.length - 1);
+		})
+		.add('factories', () => {
+			factoriesA(series.length - 1);
+			factoriesB(series.length - 1);
+		})
+		.add('lexical', () => {
+			lexicalA(series.length - 1);
+			lexicalB(series.length - 1);
+		});
+});
